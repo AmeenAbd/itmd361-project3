412de51edac652dc170cb400cc22f6ff9c120efe
AmeenAbd <aabdrabo@hawk.iit.edu>
2022-04-14 22:47:16 -0500

    changed css and js of map

--- basicmap.js
+++ basicmap.js
@@ -1,5 +1,5 @@
 function initMap(){
- var map = new google.maps.Map(document.getElementById('map'), {
+ var map = new google.maps.Map(document.getElementById('canvas'), {
      center: {lat: -34.397, lng: 150.644},
      zoom: 8
    });
